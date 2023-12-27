9bbd7f748eb1ba17085a99b506c01f08bee7481e
ryan Kim <ryangwooz@gmail.com>
2023-12-27 13:33:10 +0900

    hello-world

---
+++ src/main/java/com/ryan/rabbitmq/config/RabbitMqConfig.java
@@ -0,0 +1,31 @@
+package com.ryan.rabbitmq.config;
+
+import com.ryan.rabbitmq.service.Tut1Receiver;
+import com.ryan.rabbitmq.service.Tut1Sender;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"tut1", "hello-world"})
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    public Queue hello(){
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    @Bean
+    public Tut1Receiver receiver() {
+        return new Tut1Receiver();
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut1Sender sender() {
+        return new Tut1Sender();
+    }
+
+}
