cfc1805928c159ce659214f6de7205c748eec8a1
ryan Kim <ryangwooz@gmail.com>
2023-12-28 09:36:07 +0900

    pub-sub

---
+++ src/main/java/com/ryan/rabbitmq/config/Tut3Config.java
@@ -0,0 +1,60 @@
+package com.ryan.rabbitmq.config;
+
+import com.ryan.rabbitmq.service.Tut3Receiver;
+import com.ryan.rabbitmq.service.Tut3Sender;
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"tut3", "pub-sub", "publish-subscribe"})
+@Configuration
+public class Tut3Config {
+
+    public static final String OBSERVATION_QUEUE = "ewsn.lwm2m.observation.ewsn-nms-group";
+
+    @Bean
+    public FanoutExchange fanout(){
+        return new FanoutExchange("tut.fanout");
+    }
+
+    @Profile("receiver")
+    private static class ReceiverConfig{
+
+        @Bean
+        public Queue autoDeleteQueue1() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Queue autoDeleteQueue2() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Queue autoDeleteQueue3(){
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Binding binding1(FanoutExchange fanout, Queue autoDeleteQueue1) {
+            return BindingBuilder.bind(autoDeleteQueue1).to(fanout);
+        }
+
+        @Bean
+        public Binding binding2(FanoutExchange fanout, Queue autoDeleteQueue2) {
+            return BindingBuilder.bind(autoDeleteQueue2).to(fanout);
+        }
+
+        @Bean
+        public Tut3Receiver receiver() {
+            return new Tut3Receiver();
+        }
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut3Sender sender() {
+        return new Tut3Sender();
+    }
+}
